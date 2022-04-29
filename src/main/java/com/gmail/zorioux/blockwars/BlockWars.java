7e24e2eeeda23158b10ee99e35874a56297f8a2a
Doluxor <54857305+Doluxor@users.noreply.github.com>
2022-04-29 19:06:12 +0300

    Added Basic methods, nothing serious here

--- src/main/java/com/gmail/zorioux/blockwars/BlockWars.java
+++ src/main/java/com/gmail/zorioux/blockwars/BlockWars.java
@@ -1,6 +1,13 @@
 package com.gmail.zorioux.blockwars;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BlockWars extends JavaPlugin{
+    @Override
+    public void onDisable() { }
+
+    @Override
+    public void onEnable() { }
 }
